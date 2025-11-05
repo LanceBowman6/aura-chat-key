7df2c9b9dc69856624ccb343acf24f2950be1cdc
LanceBowman6 <tuexieko1002@outlook.com>
2025-11-05 13:10:03 -0800

    feat(tasks): add --with-balances flag to accounts task

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,9 +1,16 @@
 import { task } from "hardhat/config";
 
-task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(`${account.address}`);
-  }
-});
+task("accounts", "Prints the list of accounts")
+  .addFlag("withBalances", "Also print balances in ETH")
+  .setAction(async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    const provider = hre.ethers.provider;
+    for (const account of accounts) {
+      if (taskArgs.withBalances) {
+        const bal = await provider.getBalance(account.address);
+        console.log(`${account.address} \t ${hre.ethers.formatEther(bal)} ETH`);
+      } else {
+        console.log(`${account.address}`);
+      }
+    }
+  });
